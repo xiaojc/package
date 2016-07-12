80835778fac86e81a4fa23a04953875ec6865637
xiaojc <xjc_sun@163.com>
2016-07-12 23:49:39 +0800

    添加echache

---
+++ ocl/src/main/java/com/rainbow/packge/ocl/demo/Demo01.java
@@ -0,0 +1,13 @@
+package com.rainbow.packge.ocl.demo;
+
+/**
+ * 功能描述:
+ * Created by Admin on 2016-07-11 22:15.
+ */
+public class Demo01 {
+    
+    public static void main(String[] args){
+        Boolean b=null;
+    }
+    
+}
